b5e240b0d02daa4f68e11773763f3d9663a20576
Akusko Danil Dmitrievich <akusko.dan@yandex.ru>
2026-05-09 15:48:49 +0700

    feat[user-service]: add FilterConfig

---
+++ user-service/src/main/java/com/example/config/FilterConfig.java
@@ -0,0 +1,22 @@
+package com.example.config;
+
+import com.example.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class FilterConfig {
+    private final JwtAuthenticationFilter filter;
+
+    @Bean
+    public FilterRegistrationBean<JwtAuthenticationFilter> jwtFilterRegistration() {
+        FilterRegistrationBean<JwtAuthenticationFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(filter);
+        registration.addUrlPatterns("/api/*");
+        registration.setOrder(1);
+        return registration;
+    }
+}
