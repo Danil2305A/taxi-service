856a81023847f728d3a971838816ddf4f4bf0f2f
Akusko Danil Dmitrievich <akusko.dan@yandex.ru>
2026-05-10 16:08:14 +0700

    feat[trip-service]: add TripRepository

---
+++ trip-service/src/main/java/com/example/repository/TripRepository.java
@@ -0,0 +1,10 @@
+package com.example.repository;
+
+import com.example.entity.Trip;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TripRepository extends JpaRepository<Trip, Long> {
+    List<Trip> findByPassengerId(Long passengerId);
+}
